5ddc3b317fbdfda62ab3de3ee358d128c6b07509
Brian Lovell <lovell.brian@gmail.com>
2026-08-13 15:48:54 +1000

    Add devcontainer configuration and install script with dependencies

---
+++ .devcontainer/install-dev-tools.sh
@@ -0,0 +1,12 @@
+#/bin/env bash
+
+# Put any custom installs in this file
+
+# Put files in USER_FOLDER. Only do this once. 
+pip install ipywidgets==7.8.0
+# Notes:
+# Commands for presentation using jupyter notebook
+# cd slides
+# jupyter nbconvert birds.ipynb --to slides --post serve
+# Jupyter notebook
+
